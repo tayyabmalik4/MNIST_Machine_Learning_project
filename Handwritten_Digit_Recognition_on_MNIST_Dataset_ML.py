cc43393b7b1e4bb7e34eb6bc1641043189d700cc
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-09-02 13:50:25 +0500

    1.0

---
+++ Handwritten_Digit_Recognition_on_MNIST_Dataset_ML.py
@@ -0,0 +1,13 @@
+# (21)*****************************Handwritten Digit Recognition on MNIST Dataset in Machine Learning***********************
+
+# ******What is MNIST?
+# ---Set of 70000 small images of digits handwritten by high school students and employees of the US Census Bureau
+# ---All images are labelled with the respective digit they represent
+# ---MNIST is the hello world of machine learning
+# ---There are 70000 images and each image has 784 features
+# ---Each image is 28X28 pixels, and each feature simply represents one pic
+
+# **************Agenda
+# ----In the last tutorials we saw how to solve an ML problem end to end
+# ----In this tutorial we will first fetch the data and then split it into train and test sets
+# ----After that we will apply few ML algorithms to detect a given digit
